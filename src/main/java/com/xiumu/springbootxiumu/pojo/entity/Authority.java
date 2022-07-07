2268f414d007e53575318d9a3df9de681ef7083f
liuyimin <liuyimin@tata.com.cn>
2022-07-07 21:09:07 +0800

    添加权限类型字段

--- src/main/java/com/xiumu/springbootxiumu/pojo/entity/Authority.java
+++ src/main/java/com/xiumu/springbootxiumu/pojo/entity/Authority.java
@@ -1,11 +1,13 @@
 package com.xiumu.springbootxiumu.pojo.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.xiumu.springbootxiumu.enums.AuthType;
 import com.xiumu.springbootxiumu.pojo.base.BaseEntity;
 import lombok.Data;
 
 /**
  * 权限
+ *
  * @author xiumu
  */
 @Data
@@ -26,5 +28,10 @@ public class Authority extends BaseEntity {
      * 权限描述
      */
     private String authDesc;
+
+    /**
+     * 权限类型：0菜单，1按钮，2接口
+     */
+    private AuthType authType;
 
 }
