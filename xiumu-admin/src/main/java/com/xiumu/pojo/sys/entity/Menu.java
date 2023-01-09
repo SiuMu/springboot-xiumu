ed2c45390ba54e8cc636bf7eb455ae805f8fcb3f
liuyimin <liuyimin@tata.com.cn>
2023-01-09 23:19:02 +0800

    添加菜单类

--- xiumu-admin/src/main/java/com/xiumu/pojo/sys/entity/Menu.java
+++ xiumu-admin/src/main/java/com/xiumu/pojo/sys/entity/Menu.java
@@ -5,17 +5,17 @@ import com.xiumu.common.core.entity.BaseEntity;
 import lombok.Data;
 
 /**
-* 
-* @TableName sys_menu
-*/
+ * 菜单表，菜单与权限是一对一的关系
+ *
+ * @author XiuMu
+ */
 @Data
 @TableName("sys_menu")
 public class Menu extends BaseEntity {
-
     /**
     * 父级菜单ID
     */
-    private String parentId;
+    private Long parentId;
     /**
     * 菜单名称
     */
